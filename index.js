3af82a3ff8a761a4fdaf28a890f6075e17f0a2cd
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-05-28 19:50:33 +0100

    fix

--- index.js
+++ index.js
@@ -299,16 +299,18 @@ client.on('interactionCreate', async (interaction) => {
 
             const embed = new Discord.MessageEmbed()
                 .setTitle('Weather Information')
-                .setDescription(`Weather information for ${reportedLocation}`)
-                .addField('Temperature', `${temperature}°C`, true)
-                .addField('Weather Description', weatherDescription, true)
-                .addField('Wind Speed', `${windSpeed} km/h`, true)
-                .addField('Wind Direction', `${windDirection}°`, true)
-                .addField('Humidity', `${relativeHumidity}%`, true)
-                .addField('Pressure at sea level', `${relativePressure}hPa`, true)
-                .addField('Cloudiness', `${cloudiness}%`, true)
-                .setImage(weatherMapImageUrl)
-                .setColor('#0099ff');
+                .addFields(
+                    { name: 'Location', value: `${reportedLocation}.`, inline: true },
+                    { name: 'Weather Description:', value: weatherDescription, inline: true },
+                    { name: 'Temperature:', value: `${temperature}°C` },
+                    { name: 'Wind Speed:', value: `${windSpeed} km/h`, inline: true },
+                    { name: 'Wind Direction:', value: `${windDirection}°`, inline: true },
+                    { name: 'Humidity:', value: `${relativeHumidity}%`, inline: false },
+                    { name: 'Pressure at sea level:', value: `${relativePressure}hPa`, inline: true },
+                    { name: 'Cloudiness:', value: `${cloudiness}%`, inline: true },
+                )
+                .setColor('#0099ff')
+                .setImage(weatherMapImageUrl);
 
             await interaction.reply({ embeds: [embed] });
         } catch (error) {
@@ -340,89 +342,73 @@ async function getWeatherData(coordinates) {
     const { lat, lng, formatted } = coordinates;
     const trimmedLat = lat.toString().trim();
     const trimmedLng = lng.toString().trim();
-    const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${trimmedLat}&longitude=${trimmedLng}&hourly=temperature_2m,relativehumidity_2m,weathercode,pressure_msl,cloudcover,windspeed_10m,winddirection_10m&forecast_days=1&timezone=auto`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${trimmedLat}&lon=${trimmedLng}&appid=${openWeatherMapApiKey}&units=metric`;
 
     try {
         const response = await axios.get(weatherUrl);
-        const { hourly, utc_offset_seconds } = response.data;
+        const { main, wind, weather } = response.data;
 
-        if (!hourly || !hourly.temperature_2m || hourly.temperature_2m.length === 0) {
-            throw new Error('Weather data not available');
-        }
-
-        const currentDateTime = new Date();
-        const adjustedDateTime = new Date(currentDateTime.getTime() + utc_offset_seconds * 1000); // Adjusting current time based on UTC offset
-        const closestTimeIndex = getClosestTimeIndex(hourly.time, adjustedDateTime.getTime());
-
-        if (closestTimeIndex === -1) {
-            throw new Error('Unable to determine closest time index');
-        }
-
-        const temperature = hourly.temperature_2m[closestTimeIndex];
-        const weatherCode = hourly.weathercode[closestTimeIndex];
-        const windSpeed = hourly.windspeed_10m[closestTimeIndex];
-        const windDirection = hourly.winddirection_10m[closestTimeIndex];
-        const relativeHumidity = hourly.relativehumidity_2m[closestTimeIndex];
-        const relativePressure = hourly.pressure_msl[closestTimeIndex];
-        const cloudiness = hourly.cloudcover[closestTimeIndex];
+        const temperature = main.temp;
+        const weatherDescription = weather[0].description;
+        const windSpeed = wind.speed;
+        const windDirection = wind.deg;
+        const relativeHumidity = main.humidity;
+        const relativePressure = main.pressure;
 
         return {
             temperature,
-            weatherDescription: getWeatherDescription(weatherCode),
+            weatherDescription,
             windSpeed,
             windDirection,
             formatted,
             relativeHumidity,
             relativePressure,
-            cloudiness,
         };
     } catch (error) {
-        console.error('Error fetching weather data from Open-Meteo API:', error);
-        throw new Error('Error fetching weather data from Open-Meteo API');
+        console.error('Error fetching weather data from OpenWeatherMap API:', error);
+        throw new Error('Error fetching weather data from OpenWeatherMap API');
     }
 }
 
 async function getWeatherMapImageUrl(coordinates) {
-  try {
     const { lat, lng } = coordinates;
-    const weatherMapUrl = `https://tile.openweathermap.org/map/precipitation_new/10/${lat}/${lng}.png?appid=${openWeatherMapApiKey}`;
+    const weatherMapUrl = `https://tile.openweathermap.org/map/precipitation_new/5/${lat}/${lng}.png?appid=${openWeatherMapApiKey}`;
 
     return weatherMapUrl;
-  } catch (error) {
-    console.error('Error fetching weather map image:', error);
-    return null;
-  }
-}
-
-function getClosestTimeIndex(timeArray, targetDateTime) {
-    let minDiff = Infinity;
-    let closestIndex = 0;
-
-    for (let i = 0; i < timeArray.length; i++) {
-        const time = timeArray[i];
-        const timeDiff = Math.abs(targetDateTime - time);
-
-        if (timeDiff < minDiff) {
-            minDiff = timeDiff;
-            closestIndex = i;
-        }
-    }
-
-    return closestIndex;
 }
 
 function getWeatherDescription(weatherCode) {
-    // Add your own weather code to description mappings here
-    const weatherDescriptions = {
+    // Define the weather code to description mappings based on WMO 4677 weather code table
+    const weatherCodeMappings = {
         0: 'Clear sky',
-        1: 'Cloudy sky',
-        2: 'Rain',
-        3: 'Thunderstorm',
-        4: 'Sleet',
-        5: 'Snow',
+        1: 'Mainly clear',
+        2: 'Partly cloudy',
+        3: 'Overcast',
+        45: 'Fog',
+        46: 'Depositing rime fog',
+        51: 'Light drizzle',
+        53: 'Moderate drizzle',
+        55: 'Dense intensity drizzle',
+        56: 'Light freezing drizzle',
+        57: 'Dense freezing drizzle',
+        61: 'Slight rain',
+        63: 'Moderate rain',
+        65: 'Heavy rain',
+        66: 'Light freezing rain',
+        67: 'Heavy freezing rain',
+        71: 'Slight snowfall',
+        73: 'Moderate snowfall',
+        75: 'Heavy snowfall',
+        77: 'Snow grains',
+        80: 'Slight rain showers',
+        81: 'Moderate rain showers',
+        82: 'Violent rain showers',
+        85: 'Slight snow showers',
+        86: 'Heavy snow showers',
+        95: 'Thunderstorm',
+        96: 'Slight hail thunderstorm',
+        99: 'Heavy hail thunderstorm',
     };
-
-    return weatherDescriptions[weatherCode] || 'Unknown';
 }
 
 client.login(token);
