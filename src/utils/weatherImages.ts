125e709d1549fbbcb60486bc8576703e59d93e64
viniciussaporto <vinicius.saporto+github@gmail.com>
2025-02-04 16:54:25 +0000

    fixing weather images wildcard (that doesnt work)

--- src/utils/weatherImages.ts
+++ src/utils/weatherImages.ts
@@ -3,91 +3,91 @@ export const getWeatherImage = (weatherCode: number): string => {
 
 	switch (weatherCode) {
 		case 0: // Clear sky
-			weatherImage = 'images/icons8-sun-*.png';
+			weatherImage = 'images/icons8-sun-48.png';
 			break;
 		case 1: // Mainly clear
-			weatherImage = 'images/icons8-partly-cloudy-day-*.png';
+			weatherImage = 'images/icons8-partly-cloudy-day-48.png';
 			break;
 		case 2: // Partly cloudy
-			weatherImage = 'images/icons8-partly-cloudy-day-*.png';
+			weatherImage = 'images/icons8-partly-cloudy-day-48.png';
 			break;
 		case 3: // Overcast
-			weatherImage = 'images/icons8-cloud-*.png';
+			weatherImage = 'images/icons8-cloud-48.png';
 			break;
 		case 45: // Fog
-			weatherImage = 'images/icons8-fog-*.png';
+			weatherImage = 'images/icons8-fog-48.png';
 			break;
 		case 46: // Depositing rime fog
-			weatherImage = 'images/icons8-haze-*.png';
+			weatherImage = 'images/icons8-haze-48.png';
 			break;
 		case 51: // Light drizzle
-			weatherImage = 'images/icons8-light-rain-*.png';
+			weatherImage = 'images/icons8-light-rain-48.png';
 			break;
 		case 53: // Moderate drizzle
-			weatherImage = 'images/icons8-moderate-rain-*.png';
+			weatherImage = 'images/icons8-moderate-rain-48.png';
 			break;
 		case 55: // Dense intensity drizzle
-			weatherImage = 'images/icons8-sun-*.png';
+			weatherImage = 'images/icons8-sun-48.png';
 			break;
 		case 56: // Light freezing drizzle
-			weatherImage = 'images/icons8-sun-*.png';
+			weatherImage = 'images/icons8-sun-48.png';
 			break;
 		case 57: // Dense freezing drizzle
-			weatherImage = 'images/icons8-sun-*.png';
+			weatherImage = 'images/icons8-sun-48.png';
 			break;
 		case 61: // Slight rain
-			weatherImage = 'images/icons8-light-rain-*.png';
+			weatherImage = 'images/icons8-light-rain-48.png';
 			break;
 		case 63: // Moderate rain
-			weatherImage = 'images/icons8-moderate-rain-*.png';
+			weatherImage = 'images/icons8-moderate-rain-48.png';
 			break;
 		case 65: // Heavy rain
-			weatherImage = 'images/icons8-rainfall-*.png';
+			weatherImage = 'images/icons8-rainfall-48.png';
 			break;
 		case 66: // Light freezing rain
-			weatherImage = 'images/icons8-sleet-*.png';
+			weatherImage = 'images/icons8-sleet-48.png';
 			break;
 		case 67: // Heavy freezing rain
-			weatherImage = 'images/icons8-sleet-*.png';
+			weatherImage = 'images/icons8-sleet-48.png';
 			break;
 		case 71: // Slight snowfall
-			weatherImage = 'images/icons8-light-snow-*.png';
+			weatherImage = 'images/icons8-light-snow-48.png';
 			break;
 		case 73: // Moderate snowfall
-			weatherImage = 'images/icons8-sun-*.png';
+			weatherImage = 'images/icons8-sun-48.png';
 			break;
 		case 75: // Heavy snowfall
-			weatherImage = 'images/icons8-sun-*.png';
+			weatherImage = 'images/icons8-sun-48.png';
 			break;
 		case 77: // Snow grains
-			weatherImage = 'images/icons8-snow-storm-*.png';
+			weatherImage = 'images/icons8-snow-storm-48.png';
 			break;
 		case 80: // Slight rain showers
-			weatherImage = 'images/icons8-light-rain-*.png';
+			weatherImage = 'images/icons8-light-rain-48.png';
 			break;
 		case 81: // Moderate rain showers
-			weatherImage = 'images/icons8-moderate-rain-*.png';
+			weatherImage = 'images/icons8-moderate-rain-48.png';
 			break;
 		case 82: // Violent rain showers
-			weatherImage = 'images/icons8-rainfall-*.png';
+			weatherImage = 'images/icons8-rainfall-48.png';
 			break;
 		case 85: // Slight snow showers
-			weatherImage = 'images/icons8-snow-*.png';
+			weatherImage = 'images/icons8-snow-48.png';
 			break;
 		case 86: // Heavy snow showers
-			weatherImage = 'images/icons8-light-snow-*.png';
+			weatherImage = 'images/icons8-light-snow-48.png';
 			break;
 		case 95: // Thunderstorm
-			weatherImage = 'images/icons8-storm-with-heavy-rain-*.png';
+			weatherImage = 'images/icons8-storm-with-heavy-rain-48.png';
 			break;
 		case 96: // Slight hail thunderstorm
-			weatherImage = 'images/icons8-storm-*.png';
+			weatherImage = 'images/icons8-storm-48.png';
 			break;
 		case 99: // Heavy hail thunderstorm
-			weatherImage = 'images/icons8-storm-with-heavy-rain-*.png';
+			weatherImage = 'images/icons8-storm-with-heavy-rain-48.png';
 			break;
 		default:
-			weatherImage = 'images/icons8-puzzled-*.png';
+			weatherImage = 'images/icons8-puzzled-48.png';
 			break;
 	}
 
